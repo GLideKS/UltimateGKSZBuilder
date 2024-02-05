d5d1593a6a4da837c94d37068c5ed05b413a8768
spherallic <spherallic@gmail.com>
2024-02-05 01:47:00 +0100

    Lower minimum sprite radius

--- Source/Core/Rendering/Renderer2D.cs
+++ Source/Core/Rendering/Renderer2D.cs
@@ -51,8 +51,8 @@ namespace CodeImp.DoomBuilder.Rendering
 		//private const float THING_CIRCLE_SIZE = 1f;
 		private const float THING_SPRITE_SHRINK = 2f;
 		private const int THING_BUFFER_SIZE = 100;
-		private const float MINIMUM_THING_RADIUS = 0.5f; //mxd
-		private const float MINIMUM_SPRITE_RADIUS = 8.0f; //mxd
+		private const float MINIMUM_THING_RADIUS = 0.5f; //used to be 1.5f
+		private const float MINIMUM_SPRITE_RADIUS = 2.0f; //used to be 8.0f
 		internal const float FIXED_THING_SIZE = 48.0f; //mxd
 
 		internal const int NUM_VIEW_MODES = 4;
