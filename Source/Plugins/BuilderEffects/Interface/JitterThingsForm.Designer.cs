dak151/UltimateDoomBuilder

FIX: Linux build labels does does not render if AutoSize is true in many cases

Source/Plugins/BuilderEffects/Interface/JitterThingsForm.Designer.cs
@@ -51,13 +51,17 @@
 			this.cbRelativeScale = new System.Windows.Forms.CheckBox();
 			this.bUpdateScaleY = new System.Windows.Forms.Button();
 			this.maxScaleYLabel = new System.Windows.Forms.Label();
+			this.maxScaleYLabel.AutoSize = true;
 			this.maxScaleY = new System.Windows.Forms.NumericUpDown();
 			this.minScaleYLabel = new System.Windows.Forms.Label();
+			this.minScaleYLabel.AutoSize = true;
 			this.minScaleY = new System.Windows.Forms.NumericUpDown();
 			this.bUpdateScaleX = new System.Windows.Forms.Button();
 			this.label3 = new System.Windows.Forms.Label();
+			this.label3.AutoSize = true;
 			this.maxScaleX = new System.Windows.Forms.NumericUpDown();
 			this.label2 = new System.Windows.Forms.Label();
+			this.label2.AutoSize = true;
 			this.minScaleX = new System.Windows.Forms.NumericUpDown();
 			this.toolTip = new System.Windows.Forms.ToolTip(this.components);
 			this.groupBox1.SuspendLayout();

GLideKS/UltimateGKSZBuilder

Source/Plugins/BuilderEffects/Interface/JitterThingsForm.Designer.cs
@@ -47,9 +47,12 @@
 			this.scalegroup = new System.Windows.Forms.GroupBox();
 			this.cbRelativeScale = new System.Windows.Forms.CheckBox();
 			this.bUpdateScale = new System.Windows.Forms.Button();
+			this.bUpdateScale.AutoSize = true;
 			this.label3 = new System.Windows.Forms.Label();
+			this.label3.AutoSize = true;
 			this.maxScale = new System.Windows.Forms.NumericUpDown();
 			this.label2 = new System.Windows.Forms.Label();
+			this.label2.AutoSize = true;
 			this.minScale = new System.Windows.Forms.NumericUpDown();
 			this.toolTip = new System.Windows.Forms.ToolTip(this.components);
 			this.groupBox1.SuspendLayout();